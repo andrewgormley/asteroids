2dc5333d99ac21fea0c3e555932eec9314e3c7fe
andrewgormley <andrewgormley91@gmail.com>
2024-12-27 17:34:52 +0000

    added movement

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     play = True
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
 
     while play:
         for event in pygame.event.get():
@@ -13,7 +21,16 @@ def main():
                 return
 
         pygame.Surface.fill(screen, (0,0,0))
+
+        for i in updatable:
+            i.update(dt)
+
+        for i in drawable:
+            i.draw(screen)
+        
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
